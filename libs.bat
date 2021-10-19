16b913436007f92714bbb5e5f033fcf7b6b49ced
unknown <ymartin014@gmail.com>
2021-10-19 18:21:01 -0300

    no remove exe

--- libs.bat
+++ libs.bat
@@ -1,7 +1,6 @@
 ::Install VS C++ Buils Tools
 powershell "Invoke-WebRequest https://aka.ms/vs/17/release/VC_redist.x64.exe -Outfile .\tools.exe"
 powershell "Start-Process .\tools.exe RunAs"
-powershell "Remove-Item .\tools.exe"
 
 ::Other libraries
 pip install yt-dlp
