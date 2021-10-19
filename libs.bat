93be65f972a6af332ea817fd9be5e2f70f62ad04
unknown <ymartin014@gmail.com>
2021-10-19 17:58:30 -0300

    add VS build tools installer

--- libs.bat
+++ libs.bat
@@ -1,9 +1,13 @@
-pip install subprocess
+::Install VS C++ Buils Tools
+powershell "Invoke-WebRequest https://aka.ms/vs/17/release/VC_redist.x64.exe -Outfile .\tools.exe"
+powershell "Start-Process .\tools.exe RunAs"
+powershell "Remove-Item .\tools.exe"
+
+::Other libraries
 pip install yt-dlp
+pip install numpy
 pip install -U discord.py
 pip install -U "discord.py[voice]"
 pip install python-dotenv
 pip install spotdl
 pip install youtube-search
-pip install platform
-
